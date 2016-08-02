6e60cc0d2713452ed29deb0e0adcd7e238bc17e7
cai-bird <1557716210@qq.com>
2016-08-03 00:15:59 +0800

    main02
    main02

--- src/practice/test/HelloEgit.java
+++ src/practice/test/HelloEgit.java
@@ -4,5 +4,7 @@ public class HelloEgit {
 	public static void main(String[] args) {
 		System.out.println("hello main!");
 		System.out.println("hello main01!");
+		System.out.println("hello main02!");
+		
 	}
 }
